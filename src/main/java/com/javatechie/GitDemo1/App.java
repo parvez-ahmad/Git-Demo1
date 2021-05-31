e3f1053cb513a0a07ff3a84728c3afa1c5ed99ec
parvez09ahmad <parveziahmad09@gmail.com>
2021-05-31 12:30:06 +0530

    added if condition to check the data

--- src/main/java/com/javatechie/GitDemo1/App.java
+++ src/main/java/com/javatechie/GitDemo1/App.java
@@ -4,13 +4,15 @@ package com.javatechie.GitDemo1;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 	public void addData(String data) {
-		System.out.println(data+"= data");
+		System.out.println(data + "= data");
+		if (data.equals("javatechie")) {
+			System.out.println("welcom");
+		}
 	}
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
